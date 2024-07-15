fc7825b6558542904841f5b904620c28084b4284
RicardoDB8 <bertarini@gmail.com>
2024-07-15 20:33:06 -0300

    Contador

--- src/Components/NavBar/index.jsx
+++ src/Components/NavBar/index.jsx
@@ -1,7 +1,10 @@
+import { useContext } from "react"
 import { NavLink } from "react-router-dom"
+import { ShoppingCartContext } from '../../Context'
 
 const NavBar = () => {
     const activeStyle = 'underline underline-offset-4'
+    const context = useContext(ShoppingCartContext)
 
     return (
         <nav className='flex justify-between items-center fixed z-10 top-0 w-full py-5 px-8 text-sm font-light'>
@@ -98,7 +101,7 @@ const NavBar = () => {
                     </NavLink>
                 </li>
                 <li>
-                    🛒 0
+                    🛒 {context.count}
                 </li>
             </ul>
         </nav>

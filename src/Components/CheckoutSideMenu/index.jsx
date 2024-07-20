4873ce66f86cb8865d385dc1e6fbf0aba3916e5f
RicardoDB8 <bertarini@gmail.com>
2024-07-20 14:02:35 -0300

    Componente Order Card

--- src/Components/CheckoutSideMenu/index.jsx
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -1,10 +1,12 @@
 import { useContext } from 'react'
 import { XMarkIcon } from '@heroicons/react/24/solid'
 import { ShoppingCartContext } from '../../Context'
+import OrderCard from '../../Components/OrderCar'
 import './styles.css'
 
 const CheckoutSideMenu = () => {
   const context = useContext(ShoppingCartContext)
+  console.log('CART: ', context.cartProducts)
 
   return (
     <aside
@@ -17,6 +19,19 @@ const CheckoutSideMenu = () => {
             onClick={() => context.closeCheckoutSideMenu()}></XMarkIcon>
         </div>
       </div>
+      <div className='px-6'>
+      {
+        context.cartProducts.map(product => (
+          <OrderCard 
+          key={product.id}
+          title={product.title} 
+          imageUrl={product.images} 
+          price={product.price} 
+          />
+        ))
+      }
+      </div>
+      
     </aside>
   )
 }

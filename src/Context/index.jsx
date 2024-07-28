ec0e2ee08dbac1cf8bff57cc36535c4b5e708219
RicardoDB8 <bertarini@gmail.com>
2024-07-28 12:19:40 -0300

    Filtrando titulos

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -27,16 +27,24 @@ export const ShoppingCartProvider = ({children}) => {
 
   // Get products
   const [items, setItems] = useState(null)
+  const [filteredItems, setFilteredItems] = useState(null)
 
-  // Get products By Title
+  // Get products by title
   const [searchByTitle, setSearchByTitle] = useState(null)
-  console.log('searchByTitle: ', searchByTitle)
 
   useEffect(() => {
     fetch('https://api.escuelajs.co/api/v1/products')
-    .then(response => response.json())
-    .then(data => setItems(data))
+      .then(response => response.json())
+      .then(data => setItems(data))
   }, [])
+
+  const filteredItemsByTitle = (items, searchByTitle) => {
+    return items?.filter(item => item.title.toLowerCase().includes(searchByTitle.toLowerCase()))
+  }
+
+  useEffect(() => {
+    if (searchByTitle) setFilteredItems(filteredItemsByTitle(items, searchByTitle))
+  }, [items, searchByTitle])
 
   return (
     <ShoppingCartContext.Provider value={{
@@ -57,7 +65,8 @@ export const ShoppingCartProvider = ({children}) => {
       items,
       setItems,
       searchByTitle,
-      setSearchByTitle
+      setSearchByTitle,
+      filteredItems
     }}>
       {children}
     </ShoppingCartContext.Provider>

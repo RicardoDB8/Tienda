fa60cc795f78d2fdfe7561dcb5c2bba89ab42e5f
RicardoDB8 <bertarini@gmail.com>
2024-07-28 11:16:53 -0300

    Buscador de productos

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from 'react'
+import { createContext, useState, useEffect } from 'react'
 
 export const ShoppingCartContext = createContext()
 
@@ -25,6 +25,19 @@ export const ShoppingCartProvider = ({children}) => {
   // Shopping Cart · Order
   const [order, setOrder] = useState([])
 
+  // Get products
+  const [items, setItems] = useState(null)
+
+  // Get products By Title
+  const [searchByTitle, setSearchByTitle] = useState(null)
+  console.log('searchByTitle: ', searchByTitle)
+
+  useEffect(() => {
+    fetch('https://api.escuelajs.co/api/v1/products')
+    .then(response => response.json())
+    .then(data => setItems(data))
+  }, [])
+
   return (
     <ShoppingCartContext.Provider value={{
       count,
@@ -40,7 +53,11 @@ export const ShoppingCartProvider = ({children}) => {
       openCheckoutSideMenu,
       closeCheckoutSideMenu,
       order,
-      setOrder
+      setOrder,
+      items,
+      setItems,
+      searchByTitle,
+      setSearchByTitle
     }}>
       {children}
     </ShoppingCartContext.Provider>
